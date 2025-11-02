d8a5c804ba44395048dcf67aee669e47e242a948
powerstephen <power.stephen@gmail.com>
2025-11-02 14:21:52 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,11 @@
 import Hero from '../components/Hero';
 import { RoiCalculator } from '../components/RoiCalculator';
 
-export default function Page(){
+export default function Page() {
   return (
-    <main>
+    <>
       <Hero />
-      <div className="container">
-        <div className="section">
-          <RoiCalculator />
-        </div>
-      </div>
-    </main>
+      <RoiCalculator />
+    </>
   );
 }
